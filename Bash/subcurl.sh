c2539f04492f62f8c9a874614ef6d336b02e814c
Dakota <dakotadunn5@gmail.com>
2023-03-14 21:08:13 -0400

    still have to clean script up, but getting there

--- Bash/subcurl.sh
+++ Bash/subcurl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-subfinder -d $1 | tee $1; mkdir $1.dir; mv $1 $1.dir; cd $1.dir;  for i in $(cat $1); do curl -X GET $i | tee $1.curl; cat $1.curl | grep -e "200" | sort -u | tee $1.domains; done;
+subfinder -d $1 | tee $1; mkdir $1.dir; mv $1 $1.dir; cd $1.dir;  cat $1 | httprobe | tee $1.domains; for line in ${cat $1.domains}; do gobuster dir -u $line -w /usr/share/seclists/SecLists-master/Discovery/Web-Content/raft-medium-words.txt | tee $1.brute; cat $1.brute | httprobe | tee $1.brute; cat $1.brute| sort | tee $1.sorteddirs; done;
 
