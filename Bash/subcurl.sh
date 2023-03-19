d559204ce59afe26ecde7090d8242170da78b02e
Dakota Dunn <dakotadunn5@gmail.com>
2023-03-18 20:25:25 -0400

    added a quicker script

--- Bash/subcurl.sh
+++ Bash/subcurl.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-subfinder -d $1 | tee $1; mkdir $1.dir; mv $1 $1.dir; cd $1.dir;  cat $1 | httprobe | tee $1.domains; for line in ${cat $1.domains}; do gobuster dir -u $line -w /usr/share/seclists/SecLists-master/Discovery/Web-Content/raft-medium-words.txt | tee $1.brute; cat $1.brute | httprobe | tee $1.brute; cat $1.brute| sort | tee $1.sorteddirs; done;
-
+subfinder -d $1 | httprobe | tee $1.domains; for i in ${cat $1.domains}; do gobuster dir -u $i -w //usr/share/seclists/SecLists-master/Discovery/Web-Content/raft-medium-words.txt | tee $1.directories; cat $1.directories | sort | tee $1.directories; done; 
